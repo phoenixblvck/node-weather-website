d29eb90a57ee6e70418e193cd193af1cf3b3493d
Andries Bingani <info@andriesbingani.co.za>
2020-07-12 21:52:33 +0200

    setup app for heroku

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geoCode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+const port = process.env.Port || 3000
 
 //Define paths for Express config
 const directoryName = path.join(__dirname, '../public');
@@ -93,6 +94,6 @@ app.get('*', (req, res) => {
     });
 });
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.');
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}.`);
 });
